f1b56b27e95806cb0bfe2c60065fc342a0e6fd9c
Guwanus <juanmanuelholoss@gmail.com>
2025-12-11 00:17:52 +0100

    terraform destroy logic

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,5 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "cs3-terraform-state-juan"   # jouw bucketnaam
+    key            = "cs3/infra/terraform.tfstate" # willekeurig pad in die bucket
+    region         = "eu-central-1"                # zelfde regio als bucket
+    dynamodb_table = "cs3-terraform-locks"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
@@ -8,9 +20,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region = var.aws_region
-}
-
-# You can optionally add a backend here (S3, etc.)
